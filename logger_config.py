4ead0bc2f01c014c4959dc3ca45eca1d9de278ec
Mikaciu <mikael.hautin@gmail.com>
2017-08-02 22:19:17 +0200

    Revamped the logging processes.

--- logger_config.py
+++ logger_config.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python3
 import logging
+
 from colorlog import ColoredFormatter
 
 
-# {color}, fg_{color}, bg_{color}: Foreground and background colors.
-# The color names are black, red, green, yellow, blue, purple, cyan and white.
-# bold, bold_{color}, fg_bold_{color}, bg_bold_{color}: Bold/bright colors.
-# reset: Clear all formatting (both foreground and background colors).
-formatter = ColoredFormatter(
-    "%(fg_yellow)s%(asctime)s%(reset)s %(log_color)s%(levelname)-8s%(reset)s [%(module)s] %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    reset=True,
-    log_colors={
-        'DEBUG': 'purple',
-        'INFO': 'green',
-        'WARNING': 'yellow',
-        'ERROR': 'red',
-        'CRITICAL': 'red,bg_white',
-        '2DO': 'blue',
-    },
-    secondary_log_colors={},
-    style='%'
-)
+class LoggerConfig:
+    LOGGER_2DO = logging.INFO + 1
 
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-# ch.setLevel(logging.NOTSET)
+    def __init__(self):
+        super()
 
-# add formatter to ch
-ch.setFormatter(formatter)
+        # {color}, fg_{color}, bg_{color}: Foreground and background colors.
+        # The color names are black, red, green, yellow, blue, purple, cyan and white.
+        # bold, bold_{color}, fg_bold_{color}, bg_bold_{color}: Bold/bright colors.
+        # reset: Clear all formatting (both foreground and background colors).
+        self.formatter = ColoredFormatter(
+            "%(fg_yellow)s%(asctime)s%(reset)s %(log_color)s%(levelname)-8s%(reset)s [%(module)s] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            reset=True,
+            log_colors={
+                'DEBUG': 'purple',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red,bg_white',
+                '2DO': 'blue',
+            },
+            secondary_log_colors={},
+            style='%'
+        )
 
-logging.basicConfig(level=logging.WARNING, handlers=[ch])
+        # create console handler and set level to debug
+        ch = logging.StreamHandler()
+
+        # add formatter to ch
+        ch.setFormatter(self.formatter)
+
+        logging.basicConfig(level=logging.WARNING, handlers=[ch])
+
+        logging.addLevelName(LoggerConfig.LOGGER_2DO, '2DO')
+
+    def getLogger(self, logger_name):
+        return logging.getLogger(logger_name)
